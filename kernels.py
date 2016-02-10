1458abfe9b3d89d21b99b8df5556efcb062f8170
Jonas Greitemann <j.greitemann@lmu.de>
2016-02-10 14:35:01 +0100

    Added domega to kernel

--- kernels.py
+++ kernels.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 def imagt_kernel(taus, omegas, beta):
-    return np.exp(-np.outer(taus, omegas)) / (1 + np.exp(-beta * omegas))
+    domega = omegas[1] - omegas[0]
+    return domega * np.exp(-np.outer(taus, omegas)) / (1 + np.exp(-beta * omegas))
 
