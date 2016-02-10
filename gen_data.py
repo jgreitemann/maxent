b3795846c50103e3371f02ea08a3fe59c6ed7d10
Jonas Greitemann <j.greitemann@lmu.de>
2016-02-10 14:34:11 +0100

    Added BCS spectral function test case

--- gen_data.py
+++ gen_data.py
@@ -14,22 +14,32 @@ def two_peaks(omegas):
     return 0.5/np.sqrt(2*np.pi)/s1 * np.exp(-0.5*(omegas-m1)**2/s1**2) \
          + 0.5/np.sqrt(2*np.pi)/s2 * np.exp(-0.5*(omegas-m2)**2/s2**2)
 
+def bcs(omegas):
+    gap = 0.91
+    W = 8.4
+    N = 1./np.sqrt(W**2/2 - gap**2)
+    return np.array([N * o / np.sqrt(o**2 - gap**2) if gap < o <= W/2 else 0.
+                     for o in omegas])
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--eta', type=float, default=1e-4)
 parser.add_argument('--beta', type=float, default=1.)
 parser.add_argument('--spectrum', action='store_true')
 parser.add_argument('--exact', action='store_true')
+parser.add_argument('--bcs', action='store_true')
 args = parser.parse_args()
+
+model = bcs if args.bcs else two_peaks
 
 taus = np.linspace(0, args.beta/2, 1001)
 omegas = np.linspace(0, 10, 1001)
 
 if args.spectrum:
-    for omega, A in izip(omegas, two_peaks(omegas)):
+    for omega, A in izip(omegas, model(omegas)):
         print omega, A
     exit(0)
 
-green = np.dot(kernels.imagt_kernel(taus, omegas, args.beta), two_peaks(omegas))
+green = np.dot(kernels.imagt_kernel(taus, omegas, args.beta), model(omegas))
 if not args.exact:
     green *= 1 + args.eta * np.random.standard_normal(green.shape)
 
